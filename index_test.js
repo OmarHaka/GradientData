0d7468bce634ee628c380a9412c259c64f9fd0fa
OmarHaka <107427573+OmarHaka@users.noreply.github.com>
2023-08-25 10:55:51 -0500

    Add files via upload

---
+++ index_test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../index'); 
+
+describe('JavaScript Hello App', () => {
+  it('should respond with "Hello Node!"', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello Node!\n');
+  });
+});
