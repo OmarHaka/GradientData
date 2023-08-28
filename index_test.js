593d3519f441ba33061b84179b34c03cb17d5eee
OmarHaka <107427573+OmarHaka@users.noreply.github.com>
2023-08-28 10:10:01 -0500

    Update index_test.js

--- index_test.js
+++ index_test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index'); 
+const app = require('./index'); 
 
 describe('JavaScript Hello App', () => {
   it('should respond with "Hello Node!"', async () => {
